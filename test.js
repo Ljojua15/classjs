58566155ae4434ac66457f9e309823b9cdba7c96
Ljojua15 <97795530+Ljojua15@users.noreply.github.com>
2023-08-10 15:42:42 +0400

    add json

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-class Car {
-  constructor(brand, model, color) {
-    this.brand = brand;
-    this.model = model;
-    this.color = color;
-  }
-}
-const myCar = new Car("subaru", "forester", "white");
-const mamCar = new Car("nissan", "tidda", "schwarz");
+// let Car = class {
+//   constructor(brand, model, color) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.color = color;
+//   }
+// };
+// const myCar = new Car("subaru", "forester", "white");
+// const mamCar = new Car("nissan", "tidda", "schwarz");
 
-console.log(myCar);
-console.log(mamCar.color);
+// console.log(myCar);
+// console.log(mamCar.color);
