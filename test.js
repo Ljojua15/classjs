6a78158ddad98b8a07f0273d23b1737b2d5f38b5
Ljojua15 <jojualasha15@gmail.com>
2023-08-28 16:35:04 +0400

    gh
    fgh

--- test.js
+++ test.js
@@ -1,12 +1,27 @@
 // let Car = class {
-//   constructor(brand, model, color) {
-//     this.brand = brand;
-//     this.model = model;
-//     this.color = color;
-//   }
+//     constructor(brand, model, color) {
+//         this.brand = brand;
+//         this.model = model;
+//         this.color = color;
+//     }
 // };
 // const myCar = new Car("subaru", "forester", "white");
 // const mamCar = new Car("nissan", "tidda", "schwarz");
 
 // console.log(myCar);
 // console.log(mamCar.color);
+
+
+class Poker {
+    constructor(kare, flush, stright) {
+        this.kare = kare;
+        this.flush = flush;
+        this.stright = stright;
+    }
+}
+
+console.log(Poker);
+
+const myPoker = new Poker(2, 'heart', "45678");
+
+console.log(myPoker);
