3e624e3ab1c8297b6d1d2420b08bf23b31042142
Ljojua15 <97795530+Ljojua15@users.noreply.github.com>
2023-08-29 12:40:51 +0400

    new
    sd

--- test.js
+++ test.js
@@ -10,3 +10,23 @@
 
 // console.log(myCar);
 // console.log(mamCar.color);
+
+class User {
+  constructor(userName, password) {
+    this.userName = userName;
+    this.password = password;
+  }
+}
+
+class Student extends User {
+  constructor(userName, password, age) {
+    super(userName, password);
+    this.age = age;
+  }
+}
+
+const lasha = new User("lasha98", "lashalasha");
+const stundentLasha = new Student("lasha98", "lashalasha", 25);
+
+console.log(lasha);
+console.log(stundentLasha);
